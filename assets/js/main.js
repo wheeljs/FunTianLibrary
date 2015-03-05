68d165865251237d73cf6f9bd685f8d55de444c2
Tian Fang <fang.tian@kingstech.com.cn>
2015-03-05 13:37:03 +0800

    Use 'LoaderButton' library to let the button become to stateful button.Adding HTML button element.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,13 +8,33 @@ require.config({
 
 require(
 	[
-		'app/views/notification-list'
+		'jquery',
+		'app/views/notification-list',
+		'app/loader-button'
 	],
-	function (NotificationList) {
+	function ($, NotificationList, LoaderButton) {
 		var notificationList = new NotificationList({
 			id: 'app-notification-list',
 			el: '#list-container'
 		});
 		notificationList.render();
+
+		var proxy = new LoaderButton($('#js-all-button'), {
+			type: LoaderButton.Types.ALL,
+			configure: {
+				1: {
+					iconClass: 'icon-save',
+					text: 'Click to load'
+				},
+				2: {
+					iconClass: 'icon-spin',
+					text: 'Loading'
+				},
+				4: {
+					iconClass: 'icon-ok',
+					text: 'Finish'
+				}
+			}
+		});
 	}
 );
