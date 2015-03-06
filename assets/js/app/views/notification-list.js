8b810f5c1f6cc5f77227598c9cb2bb139397a525
Tian Fang <fang.tian@kingstech.com.cn>
2015-03-06 10:13:18 +0800

    Add 'NotificationList' comments.

--- assets/js/app/views/notification-list.js
+++ assets/js/app/views/notification-list.js
@@ -7,11 +7,29 @@ define(
         'app/templates'
     ],
     function (Backbone, Templates) {
+        /**
+         * 通知列表视图，仅作AMD Demo使用。
+         *
+         * @module NotificationList
+         */
         return Backbone.View.extend({
             template: Templates['notifications'],
+            /**
+             * 初始化视图时会调用该方法。
+             *
+             * @protected
+             * @param {Object} options 初始化视图的选项。
+             * @param {Object} options.data 渲染视图使用到的数据。
+             */
             initialize: function (options) {
                 this.options = options;
             },
+            /**
+             * 渲染视图。
+             *
+             * @public
+             * @returns {NotificationList}
+             */
             render: function () {
                 var options = this.options;
                 var data = options.data;
