68d0dd3ed3a2179de48022a2f47a986ffd89c14b
轱辘是神棍 <ttvip88888888@gmail.com>
2015-04-10 12:11:51 +0800

    Comments modified.

--- assets/js/app/common.js
+++ assets/js/app/common.js
@@ -15,6 +15,7 @@
          * @param {boolean} canBubble 指定事件是否可以冒泡。
          * @param {boolean} cancelBubble 指定事件是否可以被取消。
          * @param {Object=} data 事件对象要携带的数据。
+         * @return {Event} 创建并完成初始化的Event对象。
          */
         createEvent: function (eventType, canBubble, cancelBubble, data) {
             var evt = document.createEvent('Events');
@@ -31,8 +32,8 @@
          * 所以当子类需要添加方法时，请在添加方法之前调用该方法实现继承关系。
          *
          * @public
-         * @param Child 子类。
-         * @param Parent 父类。
+         * @param {Object} Child 子类。
+         * @param {Object} Parent 父类。
          */
         extend: function (Child, Parent) {
             var F = function () {
