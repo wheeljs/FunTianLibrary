7793fa212bd85d8fc87bc8da8a362fe0bc6f3dc5
轱辘是神棍 <ttvip88888888@gmail.com>
2015-04-21 17:52:48 +0800

    Added 'requirejs.config' call in all test pages. This file can help requirejs to point to the correct path in tests.

---
+++ test/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Fang on 2015/4/21.
+ *
+ * 在测试文件夹中使requirejs指向正确的路径。
+ */
+(function (requirejs) {
+    if (typeof requirejs !== 'undefined') {
+        requirejs.config({
+            baseUrl: '../assets/js/',
+            paths: {
+                jquery: 'jquery-1.9.1.min'
+            }
+        });
+    }
+})(requirejs);
