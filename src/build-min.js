f5ca70df38e2df110f00ec091082abde549aa93a
轱辘是神棍 <ttvip88888888@foxmail.com>
2015-06-07 11:29:03 +0800

    Modify r.js build file. Add minimum version build file.

---
+++ src/build-min.js
@@ -0,0 +1,8 @@
+({
+    baseUrl: "./app",
+    paths: {
+        jquery: "empty:"
+    },
+    name: "fun",
+    out: "../lib/fun-library-0.1-min.js"
+})
--- src/build.js
+++ src/build.js
@@ -1,8 +1,13 @@
 ({
-    baseUrl: ".",
+    /*
+     *  TODO 应该有一个模块，require('fun') 返回所有该库的可用功能，比如Fun.Common, Fun.Cache等等
+     *  现在做的只是定义了模块的功能和依赖，而没有将所有模块整合，打包之后的代码同样有此问题。可以参考jquery的实现。
+     */
+    baseUrl: "./app",
     paths: {
         jquery: "empty:"
     },
-    name: "modules-list",
+	optimize: "none",
+    name: "fun",
     out: "../lib/fun-library-0.1.js"
 })
